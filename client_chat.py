4313fe48f6b07d58f272cf3f5d507a9efd98b4aa
naianis <35470193+naianis@users.noreply.github.com>
2020-11-15 22:47:02 -0300

    Update client_chat.py

--- client_chat.py
+++ client_chat.py
@@ -2,39 +2,38 @@
 import sys
 import socket
 
-#SERVER_ADDRESS = '127.0.0.1'     # Endereco IP do Servidor
-#print 'Argument List:', str(sys.argv)
-#print sys.argv[0], sys.argv[1],sys.argv[2], sys.argv[3]
 
 CLIENT_NAME = sys.argv[1]           # Nome do cliente
 SERVER_ADDRESS = sys.argv[2]        # Endereco IP do Servidor
 SERVER_PORT = int(sys.argv[3])      # Porta que o Servidor esta
 
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-dest = (SERVER_ADDRESS, SERVER_PORT)
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Criacao do soquete
+dest = (SERVER_ADDRESS, SERVER_PORT) # Encapsulamos o endereço fornecido
 
-tcp.connect(dest)
-tcp.send (CLIENT_NAME)
+tcp.connect(dest)                    # Criação da conexão
+tcp.send (CLIENT_NAME)               # A primeira mensagem é sempre o nome do cliente
 
-print 'Para sair use CTRL+X\n'
-msg = raw_input()
+print 'Para sair use CTRL+C\n'
+msg = raw_input()                    # Input da mensagem a ser enviada
 
-while msg <> '\x18':
-    tcp.send (msg)
+while msg <> '\x18':                # Loop responsável por enviar e receber mensagens
+    tcp.send (msg)                  # Envia a requisição ao servidor
     
     if msg == 'WHO' or msg == 'HELP':
-        #tcp.send (msg)
-        server_response = tcp.recv(1024)
+        server_response = tcp.recv(1024)    # Recebe a resposta do servidor. Problema: se não receber, trava.
         print server_response
-        msg = raw_input()
+        msg = raw_input()                   # Input da mensagem a ser enviada
+
+    elif 'SEND' in msg:
+        server_response = tcp.recv(1024)    # Recebe a resposta do servidor
+        print server_response
+        msg = raw_input()                   # Input da mensagem a ser enviada
+    
 
     else:
         print('Mensagem nao suportada. Digite HELP para mais informações.')
         msg = raw_input()
 
 
-
-
-
     
-tcp.close()
+tcp.close()         # Fecha a conexão
